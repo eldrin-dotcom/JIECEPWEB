11ece3a97af7103d9769b3d67f2cd79dd9162aad
eldrin <eldrin.rosales22@gmail.com>
2025-07-11 01:52:22 +0800

    fix

--- src/sanityClient.js
+++ src/sanityClient.js
@@ -2,8 +2,8 @@
 import { createClient } from '@sanity/client';
 
 export const client = createClient({
-  projectId: 'vsql81qd',         // ✅ Your project ID
-  dataset: 'staging',            // ✅ Your dataset name
+  projectId: '22b59vd4',         // ✅ Your project ID
+  dataset: 'production',            // ✅ Your dataset name
   apiVersion: '2023-01-01',      // ✅ Use the same version everywhere
   useCdn: true,                  // ✅ Use CDN for public/readonly queries
 });
